4fe5ed30ce858167edf53747a1ca02f189ab6981
RoyBoy189 <64163922+RoyBoy189@users.noreply.github.com>
2022-08-25 10:56:50 -0500

    Add files via upload

---
+++ Computer sceince notes/src/App.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+import javax.print.attribute.HashAttributeSet;
+
+//Hashmap class writtren by the 2022 ADS class
+//Methods to add,remove,query
+//Hashmaps will always be unordered for efficienty
+
+public class NewHashmap {
+    public static void main(String[] args) {
+        // Demo of built-in HashMap Class
+        HashMap<String, Integer> x = new HashMap<>();
+        x.put("Andi", 100);
+        x.put("Bob", 200);
+    }
+
+}
